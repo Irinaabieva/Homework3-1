98758bca3087e98a21b5ad3a8a3dec738353e2a5
321ru <gerarr999@yandex.ru>
2022-10-31 15:22:54 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 2147483647;
+        byte b = 127;
+        short s = -32768;
+        long l =-9223372036854775808L;
+        float f = 1.5f;
+        double d = 2.6;
+
+        System.out.println("int = "+i);
+        System.out.println("byte = "+b);
+        System.out.println("short = "+s);
+        System.out.println("long = "+l);
+        System.out.println("float = "+f);
+        System.out.println("double = "+d);
+
+    }
+}
