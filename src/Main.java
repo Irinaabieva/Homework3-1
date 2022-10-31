e73d621719b41c5820d005f994547c563fea8756
321ru <gerarr999@yandex.ru>
2022-10-31 16:07:55 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        float a = 27.12f;
-        long b = 987678965549L;
-        byte c = 2;
-        short d =786;
-        boolean e = false;
-        short f = 786;
-        short g =-159;
-        short p =27897;
-        byte r = 67;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(p);
-        System.out.println(r);
+        byte ludmilaPavlova = 23;
+        byte annaSergeevna = 27;
+        byte ekaterinaAndreevna =30;
+        int allStudents = ludmilaPavlova + annaSergeevna + ekaterinaAndreevna;
+        short papaer = 480;
+        int paperStudent = papaer / allStudents;
+        System.out.println("На каждого ученика рассчитано "+paperStudent+ " листов бумаги");
+
 
     }
 }
